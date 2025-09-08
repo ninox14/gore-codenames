31baeb18c4c00ddc59d96966a2801ac36ad9f850
NiNoX14 <hardstuckd5lul@gmail.com>
2025-09-08 18:15:22 +0300

    init: init with go blueprint

---
+++ internal/server/routes.go
@@ -0,0 +1,76 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"fmt"
+	"time"
+
+	"github.com/coder/websocket"
+	"github.com/ninox14/gore-codenames/internal/middleware"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	mux := http.NewServeMux()
+
+	// Register routes
+	mux.HandleFunc("/", s.HelloWorldHandler)
+
+	mux.HandleFunc("/health", s.healthHandler)
+
+	mux.HandleFunc("/websocket", s.websocketHandler)
+
+	mws := s.CreateMWStack(middleware.CorsMiddleware)
+	// Wrap the mux with CORS middleware
+	return mws(mux)
+}
+
+func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{"message": "Hello World"}
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := w.Write(jsonResp); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
+}
+
+func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := json.Marshal(s.db.Health())
+	if err != nil {
+		http.Error(w, "Failed to marshal health check response", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := w.Write(resp); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
+}
+
+// TODO: Use socket io
+func (s *Server) websocketHandler(w http.ResponseWriter, r *http.Request) {
+	socket, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		http.Error(w, "Failed to open websocket", http.StatusInternalServerError)
+		return
+	}
+	defer socket.Close(websocket.StatusGoingAway, "Server closing websocket")
+
+	ctx := r.Context()
+	socketCtx := socket.CloseRead(ctx)
+
+	for {
+		payload := fmt.Sprintf("server timestamp: %d", time.Now().UnixNano())
+		if err := socket.Write(socketCtx, websocket.MessageText, []byte(payload)); err != nil {
+			log.Printf("Failed to write to socket: %v", err)
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+}
