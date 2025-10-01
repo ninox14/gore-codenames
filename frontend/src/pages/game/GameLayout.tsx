1a6bf40894cffb2f97c65dee003af16ceddb1983
NiNoX14 <hardstuckd5lul@gmail.com>
2025-10-01 22:13:24 +0300

    dev: prettier tailwind class sorting

--- frontend/src/pages/game/GameLayout.tsx
+++ frontend/src/pages/game/GameLayout.tsx
@@ -1,22 +1,22 @@
-import AuthDialog from '@/components/AuthDialog';
-import { useContext } from 'react';
-import { Outlet } from 'react-router';
-import { AuthContext } from '../AuthContext';
-import { getLSToken } from '@/lib/utils';
-import { GameContextProvider } from './GameContext';
-import Header from './Header';
+import AuthDialog from "@/components/AuthDialog";
+import { useContext } from "react";
+import { Outlet } from "react-router";
+import { AuthContext } from "../AuthContext";
+import { getLSToken } from "@/lib/utils";
+import { GameContextProvider } from "./GameContext";
+import Header from "./components/Header";
 
 function GameLayout() {
   const { user } = useContext(AuthContext);
   const token = getLSToken();
 
-  console.log('User', user);
+  console.log("User", user);
   return (
     <>
       <AuthDialog open={!user && !token} />
       <GameContextProvider user={user}>
         <Header />
-        <main className="min-h-screen bg-black py-12 px-4 sm:px-6 lg:px-8">
+        <main className="min-h-[calc(100vh-48px)] bg-black">
           <Outlet />
         </main>
       </GameContextProvider>
--- frontend/src/pages/game/components/Header.tsx
+++ frontend/src/pages/game/components/Header.tsx
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
-import { GameContext } from './GameContext';
-import { Button } from '@/components/ui/button';
-import { Link, useParams } from 'react-router';
+import { useContext } from "react";
+import { GameContext } from "../GameContext";
+import { Button } from "@/components/ui/button";
+import { Link, useParams } from "react-router";
 
 function Header() {
   const { isConnected, close } = useContext(GameContext);
@@ -9,14 +9,14 @@ function Header() {
   const { gameId } = useParams();
 
   return (
-    <header className="h-12 flex items-center justify-between px-5">
-      <Link className="font-bold block text-2xl" to={'/game'}>
+    <header className="flex h-12 items-center justify-between px-5">
+      <Link className="block text-2xl font-bold" to={"/game"}>
         Game
       </Link>
       {gameId && (
         <>
           <p className="grow text-center">
-            Status: {isConnected ? '🟢 Connected' : '🔴 Disconnected'}
+            Status: {isConnected ? "🟢 Connected" : "🔴 Disconnected"}
           </p>
           <Button className="block cursor-pointer" onClick={close}>
             CLOSE CONNECTION
